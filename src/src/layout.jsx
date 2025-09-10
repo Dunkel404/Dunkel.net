b4fb8c54c67fc11f512a8d007f56ae25a0847a2d
Dunkel <89481059+Dunkel404@users.noreply.github.com>
2025-09-10 19:48:53 +0000

    next

---
+++ src/src/layout.jsx
@@ -0,0 +1,11 @@
+import Home from "@/app/page"
+
+export default function main ({children}){
+    return (
+        <html>
+            <body>
+                <Home/>
+            </body>
+        </html>
+    )
+}
---
+++ src/src/page.jsx
@@ -0,0 +1,8 @@
+export default function home() {
+    return (
+        <div>
+            <h1> Texto Comum </h1>
+            <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+        </div>
+    )
+}
